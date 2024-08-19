5d8795bfe7d302552d1e98dae2014887b2ebfe67
Avneet Singh <avneetsingh@Avneets-MacBook-Pro.local>
2024-08-19 00:30:05 -0400

    formmatting

--- main.cpp
+++ main.cpp
@@ -9,23 +9,19 @@ int cellSize = 30;
 int cellCount = 25;
 
 class Food {
-    
-    public:
-        Vector2 position = {5, 6};
+ public:
+  Vector2 position = {5, 6};
 
-
-        void Draw() {
-            DrawRectangle(position.x * cellSize, position.y * cellSize, cellSize, cellSize, darkPurple);
-
-
-        }
-
+  void Draw() {
+    DrawRectangle(position.x * cellSize, position.y * cellSize, cellSize,
+                  cellSize, darkPurple);
+  }
 };
 
 int main() {
   std::cout << "Starting the game... " << '\n';
 
-  InitWindow(cellSize*cellCount, cellSize*cellCount, "Snake");
+  InitWindow(cellSize * cellCount, cellSize * cellCount, "Snake");
   SetTargetFPS(60);
   Food food = Food();
 
