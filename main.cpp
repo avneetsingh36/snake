1b6a2539f26775f4370fc082033d52ff4a1ae5ea
Avneet Singh <avneetsingh@Avneets-MacBook-Pro.local>
2024-08-27 19:10:33 -0400

    added randomized food shit

--- main.cpp
+++ main.cpp
@@ -10,11 +10,26 @@ int cellCount = 25;
 
 class Food {
  public:
-  Vector2 position = {5, 6};
+  Vector2 position;
+  Texture2D texture;
+
+  Food() {
+    Image image = LoadImage("graphics/food.png");
+    texture = LoadTextureFromImage(image);
+    UnloadImage(image);
+    position = GenerateRandomPos();
+  }
+
+  ~Food() { UnloadTexture(texture); }
 
   void Draw() {
-    DrawRectangle(position.x * cellSize, position.y * cellSize, cellSize,
-                  cellSize, darkPurple);
+    DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
+  }
+
+  Vector2 GenerateRandomPos() {
+    float x = GetRandomValue(0, cellCount - 1);
+    float y = GetRandomValue(0, cellCount - 1);
+    return Vector2{x, y};
   }
 };
 
